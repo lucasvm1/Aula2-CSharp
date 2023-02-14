04725f20c70315e394ef659616554c9103a51770
Lucas Viana <93959835+lucasvm1@users.noreply.github.com>
2023-02-14 19:34:43 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua idade");
+        var idade = Console.ReadLine();
+
+        if ((Convert.ToInt32(idade) >= 12))
+        {
+            Console.WriteLine("Voce pode jogar");
+        }
+        else
+        {
+            Console.WriteLine("Jogue outro jogo!");
+        }
+    }
+}
